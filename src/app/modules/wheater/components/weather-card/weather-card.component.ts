86929c76f5186a6516b4c0388635fa46db21a24d
Raul Antunes <raul.antunes@jupiter.co.ao>
2024-12-06 14:15:20 +0100

    Projecto 1 finalizado com sucesso

---
+++ src/app/modules/wheater/components/weather-card/weather-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { WeatherDatas } from 'src/app/modules/interfaces/WeatherDatas';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+})
+export class WeatherCardComponent {
+  @Input() weatherDatasInput!:WeatherDatas
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+}
